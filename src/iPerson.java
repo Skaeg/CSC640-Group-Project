d96456af4728a48dcc26c94e9047c03abca54b10
Skaeg <Steven.Koxlien@gmail.com>
2013-10-13 09:04:57 -0700

    Initial project creation.

---
+++ src/iPerson.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Steve
+ * Date: 10/13/13
+ * Time: 10:54 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface iPerson
+{
+}
