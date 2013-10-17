460f3eab45fb34b182d71de3e083bf3d5868af5d
Skaeg <Steven.Koxlien@gmail.com>
2013-10-16 17:48:36 -0700

    Added iPerson implementation.

--- src/Member.java
+++ src/Member.java
@@ -5,6 +5,30 @@
  * Time: 4:31 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Member
+public class Member implements iPerson
 {
+
+    @Override
+    public String getName()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void setName(String name)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public StreetAddress getAddress()
+    {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void setAddress(StreetAddress address)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
