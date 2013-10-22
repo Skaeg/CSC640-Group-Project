64a50090bf40117eed02b5c6e13896c54fe0d5d6
suespez <susan.retzer@gmail.com>
2013-10-22 16:54:15 -0700

    Setting up these classes based on our 10/20 meeting.  Once again, not positive they are as they should be.  Any corrections or another look from you would probably be good.

---
+++ src/DataProcessingController.java
@@ -0,0 +1,7 @@
+public class DataProcessingController implements iRequestReport
+{
+    @Override
+    public void requestReport(int reportType) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ src/TerminalInputOutput.java
@@ -0,0 +1,14 @@
+public class TerminalInputOutput implements iTerminal
+{
+    @Override
+    public String getInput(String str) {
+        //To change body of implemented methods use File | Settings | File Templates.
+        return null;
+    }
+
+    @Override
+    public String sendOutput(String str) {
+        //To change body of implemented methods use File | Settings | File Templates.
+        return null;
+    }
+}
